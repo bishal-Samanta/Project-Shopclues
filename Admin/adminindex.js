cc23f2f22d1da1d2bd57cdce3d74a3cd23a1e286
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2021-12-15 16:19:50 +0530

    Admin step 1 complete
    1. Admin show product page
    2. admin index.css
    3. admin index.html adminindex .js done
    4. Data store in local storage done

--- Admin/adminindex.js
+++ Admin/adminindex.js
@@ -1,3 +1,50 @@
 
 
-console.log("Test");
+
+document.querySelector("#showproduct").addEventListener("click",function(){
+    window.location.href = "admin-show-product.html";
+})
+
+
+
+
+var arrofobj = JSON.parse(localStorage.getItem("lsproductdetails")) || [];
+
+//console.log("Test");
+
+document.querySelector("#addproductfrm").addEventListener("submit",clicksubmitfunction);
+
+function clicksubmitfunction(event){
+    event.preventDefault();
+    console.log("Test");
+
+    //catch the value of inputs
+    var prodname = document.querySelector("#itemName").value;
+    var prodmrp = document.querySelector("#itemMrp").value;
+    var proddiscount = document.querySelector("#itemDiscount").value;
+    var prodcolor = document.querySelector("#itemColor").value;
+    var prodbrand = document.querySelector("#itemBrand").value;
+    var prodsize = document.querySelector("#itemSize").value;
+    var prodimage = document.querySelector("#itemimg").value;
+
+    var prodprice = prodmrp - ((prodmrp * proddiscount)/100);
+
+    var obj = {
+        name: prodname,
+        imgURL:prodimage,
+        mrp: prodmrp,
+        discount: proddiscount,
+        price:prodprice,
+        color_family:prodcolor,
+        brand: prodbrand,
+        size: prodsize
+    }
+
+    //console.log(obj);
+
+    arrofobj.push(obj);
+    //console.log(arrofobj);
+
+    localStorage.setItem("lsproductdetails", JSON.stringify(arrofobj));
+
+}
