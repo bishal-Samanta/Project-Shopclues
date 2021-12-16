d3745df9eb4f9b213225acebaba9274129be7a7a
sumeet-shinde <sam99sumit@gmail.com>
2021-12-16 18:34:48 +0530

    address, cart, car1 push
    Pushing address, cart, cart1 page data

---
+++ cart/cart.js
@@ -0,0 +1,17 @@
+data = [
+    {
+        name: "Solid Polo Classy Tshirt For Men",
+        imgURL: "https://cdn.shopclues.com/images1/thumbnails/109145/280/1/150573840-109145405-1597763799.jpg",
+        mrp: "₹499",
+        price: "₹250",
+        discount: "50%",
+        color_family: "orange",
+        brand: "Polo",
+        size: 'S',
+    }
+]
+
+
+
+    
+
