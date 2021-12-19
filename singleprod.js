138073e2e590d308d0c5482b05ab1966c1769cc7
sumeet-shinde <sam99sumit@gmail.com>
2021-12-19 18:28:09 +0530

    Update strike price

--- singleprod.js
+++ singleprod.js
@@ -40,7 +40,7 @@ AllArr.map(function(elem){
     image_bot_3.setAttribute("src", "https://cdn.shopclues.com/images1/detailed/109145/150573840-109145407-1597763801.jpg")
     document.querySelector("#MRP").textContent = elem.mrp;
     document.querySelector("#Discount").textContent = elem.discount;
-    var rP = elem.price + 26;
+    var rP = elem.price + 36;
     document.querySelector("#remPrice").textContent = rP;
     document.querySelector("#rPrice").textContent = rP;
     var dealpp = document.querySelectorAll("#DealPrice")
